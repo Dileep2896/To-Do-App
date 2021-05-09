e1a37d7f422ce5882941df21b988b95cd5bdfd1a
dileep <dileepkumar2896@gmail.com>
2021-05-09 14:34:27 +0530

    Create SQLite DatabaseHelper, Add Insert Data, Update Data, Delete Data. Create new DatabaseHelper.java class to manage the database. Add List View To Show Data

--- app/src/main/java/com/technologybit/todo/CategoryActivity.java
+++ app/src/main/java/com/technologybit/todo/CategoryActivity.java
@@ -1,38 +1,115 @@
 package com.technologybit.todo;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class CategoryActivity extends AppCompatActivity implements DialogAdd.DialogAddListener {
 
-    String newCategory;
+    ListView listViewCategory;
+    ArrayList<String> categoryList;
+    ArrayAdapter<String> arrayAdapter;
+    DatabaseHelper db;
 
+    @SuppressLint("CommitPrefEdits")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        listViewCategory = findViewById(R.id.listViewCategory);
+        db = new DatabaseHelper(this);
+
+        categoryList = new ArrayList<String>();
+        retrieveData();
+
+        listViewCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String cName = adapterView.getItemAtPosition(i).toString();
+
+                deleteData(cName);
+            }
+        });
+
     }
 
+    // Opening Dialog Box When Add is clicked
     public void addButtonClicked(View view) {
         openDialog();
     }
+    // Back Button Function
     public void goBackClicked(View view) {
         finish();
     }
 
+    // Opening Dialog Box From DialogAdd Class
     public void openDialog() {
         DialogAdd dialogAdd = new DialogAdd();
         dialogAdd.show(getSupportFragmentManager(), "Add Category");
     }
 
+    // Getting Text From The Dialog Box
     @Override
     public void addTexts(String Category) {
+        storeData(Category);
+    }
+
+    public void storeData(String Category) {
+        if (!Category.equals("") && db.insertData(Category)) {
+            Log.i("Category", "DATA ADDED");
+            categoryList.clear();
+            retrieveData();
+        } else {
+            Log.i("Category", "DATA NOT ADDED");
+        }
+    }
+
+    public void retrieveData() {
+        Cursor cursor = db.viewData();
+
+        if(cursor.getCount() == 0) {
+            Toast.makeText(CategoryActivity.this, "No Data To Show",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                categoryList.add(cursor.getString(1));
+            }
+        }
+
+        arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, categoryList);
+        listViewCategory.setAdapter(arrayAdapter);
 
     }
+
+    public void deleteData(String name) {
+        Cursor cursor = db.getItemID(name);
+        int itemID = -1;
+        while (cursor.moveToNext()) {
+            itemID = cursor.getInt(0);
+        }
+        if (itemID > -1) {
+            Log.i("Got ID: ", Integer.toString(itemID));
+            db.deleteData(itemID, name);
+            categoryList.clear();
+            retrieveData();
+        } else {
+            Log.i("Got ID: ", "NOT GOT ID");
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/technologybit/todo/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.technologybit.todo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "Category.db";
+    private static final String DB_TABLE = "Category_Table";
+
+    //columns
+    private static final String ID = "ID";
+    private static final String C_NAME = "C_NAME";
+
+    private static final String CREATE_TABLE = "CREATE TABLE " + DB_TABLE + " (" +
+            ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            C_NAME + " TEXT " + ")";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
+
+        onCreate(sqLiteDatabase);
+    }
+
+    // create methods to insert the data
+    public boolean insertData(String categoryName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(C_NAME, categoryName);
+
+        long result = db.insert(DB_TABLE, null, contentValues);
+
+        return result != -1;
+    }
+
+    // create methods to view data from database
+    public Cursor viewData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + DB_TABLE;
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+    }
+
+    // create method to get id
+    public Cursor getItemID(String cName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + ID + " FROM " + DB_TABLE
+                + " WHERE " + C_NAME + " = '" + cName + "'";
+        Cursor cursor = db.rawQuery(query, null);
+        return cursor;
+    }
+
+    // create method to delete data
+    public void deleteData(int id, String cName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + DB_TABLE + " WHERE " + ID +
+                " = '" + id + "' AND " + C_NAME + " = '" + cName + "'";
+        db.execSQL(query);
+    }
+}
