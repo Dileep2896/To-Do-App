0c7596f5cbc9ac8b4e493e14b6a435dd5c30c20a
dileep <dileepkumar2896@gmail.com>
2021-05-18 19:20:59 +0530

    Cleared Warnings and Add Some Vector Assets.

--- app/src/main/java/com/technologybit/todo/MyCustomListAdapter.java
+++ app/src/main/java/com/technologybit/todo/MyCustomListAdapter.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
